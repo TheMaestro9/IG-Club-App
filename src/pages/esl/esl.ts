457a59dc106651d9e51a04b5ffe2163dfe531754
RababAbdelWahab <rabababdelwahab21@gmail.com>
2018-04-14 13:24:32 +0200

    Add ESl Course Page

---
+++ src/pages/esl/esl.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+
+@Component({
+  selector: 'page-esl',
+  templateUrl: 'esl.html'
+})
+export class EslPage{
+  Selection; 
+  Increase;
+  
+  constructor(public navCtrl: NavController) {
+   
+
+
+  }
+
+
+
+
+
+}
