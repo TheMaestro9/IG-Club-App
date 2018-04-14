c7fa116bb289c64fd032f9c509211990594c0c98
RababAbdelWahab <rabababdelwahab21@gmail.com>
2018-04-14 17:58:08 +0200

    ESL Courses Form

--- src/pages/esl/esl.ts
+++ src/pages/esl/esl.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage,NavParams } from 'ionic-angular';
 
 
 @IonicPage() 
@@ -11,14 +11,49 @@ import { NavController, IonicPage } from 'ionic-angular';
 export class EslPage{
   Selection; 
   Increase;
-  
-  constructor(public navCtrl: NavController) {
+  time ;
+  currentRound='current Round';
+  nextRoundTime='2/5/2018';
+  cost = '2000';
+  zayed;
+  newcairo;
+  boolArr = [false , false , false , false] 
+  Names = ['zayed', 'new' , 'helo' , 'man']
+  otherCheckBox ; 
+  otherText ; 
+  constructor(public navCtrl: NavController,public navParams: NavParams)
+   {
    
 
 
   }
 
+  makeObj ( ){ 
+    var obj = {
+      'time':this.time,
+      'area':''
 
+    };
+    console.log(obj)
+
+    console.log('len' , this.boolArr.length)
+    for ( var i = 0  ; i < this.boolArr.length ; i++ ){
+      if(this.boolArr[i])
+        obj.area+=this.Names[i] + ','
+      console.log('ohhh')
+    }
+
+    console.log(obj)
+    if(this.otherCheckBox)
+        obj.area+=this.otherText 
+      else 
+        obj.area = obj.area.slice(0, obj.area.length-1)
+
+    console.log(obj)
+
+
+    console.log('ya kber' , this.boolArr)
+  }
 
 
 
